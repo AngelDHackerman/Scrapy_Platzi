4e52a806f179654e0edf778b3b1c99915acf7fcd
angel hernandez <angel.hernandez_santiago@telus.com>
2023-07-05 09:27:49 -0600

    creando archivos json y csv con los datos descargados usando scrapy

--- tutorial/quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
+++ tutorial/quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -9,24 +9,12 @@ class QuotesSpider(scrapy.Spider):
   start_urls = ['https://quotes.toscrape.com/']
 
   def parse(self, response):
-    print('*' * 10)
-    print('\n\n')
-
     title = response.xpath('//h1/a/text()').get()
-    print(f'Titulo: {title}')
-    print('\n\n')
-    print('*' * 10)
-
     quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
-    print('Citas: ')
-    for quote in quotes:
-      print(f'- {quote}')
-    print('\n\n')
-    print('*' * 10)
-
     top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
-    print('Top Ten Tags: ')
-    for tag in top_ten_tags:
-      print(f'- {tag}')
-    print('\n\n')
-    print('*' * 10)
+
+    yield { 
+      'title': title,
+      'quotes': quotes,
+      'top_ten_tags': top_ten_tags
+    }
