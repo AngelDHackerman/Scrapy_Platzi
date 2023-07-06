bef1258040ba62db9f362a55c3ce9ce0ff850518
angel hernandez <angel.hernandez_santiago@telus.com>
2023-07-06 08:37:09 -0600

    usando el metodo response.follow para seguir links con scrapy

--- tutorial/quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
+++ tutorial/quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -3,10 +3,15 @@ import scrapy
 # Titulo = //h1/a/text()
 # Citas = //span[@class="text" and @itemprop="text"]/text()
 # top ten tags = //div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()
+# Next page button = //ul[@class="pager"]//li[@class="next"]/a/@href
 
 class QuotesSpider(scrapy.Spider):
   name = 'quotes'
   start_urls = ['https://quotes.toscrape.com/']
+  custom_settings = { 
+    'FEED_URI': 'quotes.json',
+    'FEED_FORMAT': 'json'
+  }
 
   def parse(self, response):
     title = response.xpath('//h1/a/text()').get()
@@ -18,3 +23,9 @@ class QuotesSpider(scrapy.Spider):
       'quotes': quotes,
       'top_ten_tags': top_ten_tags
     }
+
+    next_page_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
+    if next_page_button_link:
+      # Con esto agregamos el endpoint relativo (/page/2/) a la ruta absoluta de la pagina (quotes.toscrape.com)
+      # el Callback function es el mismo metodo parse del generador
+      yield response.follow(next_page_button_link, callback=self.parse)
